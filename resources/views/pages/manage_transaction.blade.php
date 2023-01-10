6fe37248e7b1e67933c49cffe657665c0e34675a
Paul Angelo Soltero <paulangelo.soltero@gmail.com>
2023-01-10 17:58:38 +0800

    Update Transaction

--- resources/views/pages/manage_transaction.blade.php
+++ resources/views/pages/manage_transaction.blade.php
@@ -217,7 +217,7 @@
         <div class="modal-dialog modal-xl">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">View Account</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Create Transaction</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form id="createTransaction" action="" method="POST" enctype="multipart/form-data">
